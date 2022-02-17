2bfd10f2c17729a27cbef8bb1a8740ddbbd1160d
Ray Li <rayli@Rays-iMac.attlocal.net>
2022-02-16 18:00:10 -0800

    add support for ios and other resolutions

--- Assets/Scripts/UI/SafeAreaCanvas.cs
+++ Assets/Scripts/UI/SafeAreaCanvas.cs
@@ -4,27 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class SafeAreaCanvas : MonoBehaviour
     {
+#pragma warning disable 0649
+        private Vector2 _factorRange = new Vector2(1.7f, 2f);
+        private float _screenWidth = 800f;
+#pragma warning restore 0649
+
         private Rect _prevSafeArea;
 
         private void UpdateCanvasSize()
         {
-            float x = Screen.safeArea.x / Screen.width;
-            float width = Screen.safeArea.width / Screen.width;
-            float y = Screen.safeArea.y / Screen.height;
-            float height = Screen.safeArea.height / Screen.height;
-            float x2 = x + width;
-            float y2 = y + height;
+            float x = Screen.safeArea.x;
+            float y = Screen.safeArea.y;
+            float w = Screen.safeArea.width;
+            float h = Screen.safeArea.height;
+
+            float deviceFactor = h / w;
+            Debug.LogFormat("device factor {0}", deviceFactor);
+            if (deviceFactor < _factorRange.x)
+            {
+                float w2 = h / _factorRange.x;
+                x += (w - w2) / 2;
+                w = w2;
+            }
+            else if (deviceFactor > _factorRange.y)
+            {
+                float h2 = _factorRange.y * w;
+                y += (h - h2) / 2;
+                h = h2;
+            }
+            x /= Screen.width;
+            y /= Screen.height;
+            w /= Screen.width;
+            h /= Screen.height;
 
             RectTransform rect = GetComponent<RectTransform>();
             rect.anchorMin = new Vector2(x, y);
-            rect.anchorMax = new Vector2(x2, y2);
+            rect.anchorMax = new Vector2(x + w, y + h);
             rect.offsetMin = Vector2.zero;
             rect.offsetMax = Vector2.zero;
+
+            Debug.LogFormat(
+                "RectTransform rect {0}", rect.rect);
+            Debug.LogFormat(
+                "RectTransform anchorMin {0}", rect.anchorMin);
+            Debug.LogFormat(
+                "RectTransform anchorMax {0}", rect.anchorMax);
+            Debug.LogFormat(
+                "RectTransform offsetMin {0}", rect.offsetMin);
+            Debug.LogFormat(
+                "RectTransform offsetMax {0}", rect.offsetMax);
+
+            float scaledWidth = _screenWidth / w;
+            CanvasScaler scaler = GetComponentInParent<CanvasScaler>();
+            scaler.referenceResolution = new Vector2(scaledWidth, scaledWidth);
+
+
         }
 
         private void Awake()
